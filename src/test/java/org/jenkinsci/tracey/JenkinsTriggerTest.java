221f01a20d1953e39c897f24533ae7a0faf425ea
Mads Nielsen <man@praqma.net>
2016-07-21 13:29:01 +0200

    Updated test

--- src/test/java/org/jenkinsci/tracey/JenkinsTriggerTest.java
+++ src/test/java/org/jenkinsci/tracey/JenkinsTriggerTest.java
@@ -6,6 +6,7 @@ import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
 import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl;
 import hudson.ExtensionList;
 import hudson.model.FreeStyleProject;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import net.praqma.tracey.broker.rabbitmq.TraceyFilter;
@@ -34,37 +35,46 @@ public class JenkinsTriggerTest {
     private String password = "test";
     private int port = 6666;
 
+    private int timeout = 60000;
+
     @Rule
     public JenkinsRule r =  new JenkinsRule();
 
     @Rule
     public TraceyRabbitMQServerRule sr = new TraceyRabbitMQServerRule(traceyHost, port);
 
+    private UsernamePasswordCredentialsImpl createCredentials() {
+        SystemCredentialsProvider scp = r.getInstance().getExtensionList(com.cloudbees.plugins.credentials.SystemCredentialsProvider.class).get(0);
+        scp.getCredentials().add(new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL, "test-id", "test-cred-description", user, password));
+        UsernamePasswordCredentialsImpl upc = (UsernamePasswordCredentialsImpl)scp.getCredentials().get(0);
+        return upc;
+    }
+
+    private FreeStyleProject createAndConfigureProject(JenkinsRule r, String jobName, UsernamePasswordCredentialsImpl creds) throws IOException {
+        FreeStyleProject p = r.createFreeStyleProject(jobName);
+        ExtensionList<TraceyGlobalConfig> t = r.getInstance().getExtensionList(TraceyGlobalConfig.class);
+        TraceyGlobalConfig tgc = t.get(TraceyGlobalConfig.class);
+        TraceyHost th = new TraceyHost(traceyHost, creds.getId(), "Test credential for test", port);
+        tgc.setConfiguredHosts(Arrays.asList(th));
+        return p;
+    }
+
     @Test
     public void givenFreeStyleJob_ShouldTrigger() throws Exception {
         // We assume that 'something' responds on port 6666. Ignored if pre-condition not met
         Assume.assumeThat(sr.isResponding(), is(true));
 
-        FreeStyleProject p = r.createFreeStyleProject("Test_Receive");
-        SystemCredentialsProvider scp = r.getInstance().getExtensionList(com.cloudbees.plugins.credentials.SystemCredentialsProvider.class).get(0);
-        scp.getCredentials().add(new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL, "test-id", "test-cred-description", "test", "test"));
-        UsernamePasswordCredentialsImpl upc = (UsernamePasswordCredentialsImpl)scp.getCredentials().get(0);
+        UsernamePasswordCredentialsImpl creds = createCredentials();
+        FreeStyleProject p = createAndConfigureProject(r, "Test_Trigger", creds);
 
-        ExtensionList<TraceyGlobalConfig> t = r.getInstance().getExtensionList(TraceyGlobalConfig.class);
-        TraceyGlobalConfig tgc = t.get(TraceyGlobalConfig.class);
-        TraceyHost th = new TraceyHost(traceyHost, upc.getId(), "Test credential for test", 6666);
-        tgc.setConfiguredHosts(Arrays.asList(th));
-
-
-        TraceyTrigger tt = new TraceyTrigger(exchange, upc.getId(), false, false, envKey, Collections.EMPTY_LIST);
+        TraceyTrigger tt = new TraceyTrigger(exchange, creds.getId(), false, false, envKey, Collections.EMPTY_LIST);
         p.getTriggers().put(new TraceyTrigger.TraceyTriggerDescriptor(), tt);
         tt.start(p, true);
 
-
-        TraceyRabbitMQSenderImpl sender = new TraceyRabbitMQSenderImpl(traceyHost, user, password, 6666);
+        TraceyRabbitMQSenderImpl sender = new TraceyRabbitMQSenderImpl(traceyHost, user, password, port);
         sender.send("Hi there we should trigger", "tracey");
 
-        r.waitUntilNoActivity();
+        r.waitUntilNoActivityUpTo(timeout);
 
         TraceyAction ta = p.getBuildByNumber(1).getAction(TraceyAction.class);
         assertNotNull(ta);
@@ -74,33 +84,25 @@ public class JenkinsTriggerTest {
     @Test
     public void givenFreeStyleJob_IgnoreMessageFilterActive() throws Exception {
         Assume.assumeThat(sr.isResponding(), is(true));
-        FreeStyleProject p = r.createFreeStyleProject("Test_Ignore_PayloadFilter");
-        SystemCredentialsProvider scp = r.getInstance().getExtensionList(com.cloudbees.plugins.credentials.SystemCredentialsProvider.class).get(0);
-        scp.getCredentials().add(new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL, "test-id", "test-cred-description", "test", "test"));
-        UsernamePasswordCredentialsImpl upc = (UsernamePasswordCredentialsImpl)scp.getCredentials().get(0);
 
-        ExtensionList<TraceyGlobalConfig> t = r.getInstance().getExtensionList(TraceyGlobalConfig.class);
-        TraceyGlobalConfig tgc = t.get(TraceyGlobalConfig.class);
-        TraceyHost th = new TraceyHost(traceyHost, upc.getId(), "Test credential for test", 6666);
-        tgc.setConfiguredHosts(Arrays.asList(th));
+        UsernamePasswordCredentialsImpl creds = createCredentials();
+        FreeStyleProject p = createAndConfigureProject(r, "Test_Ignore_PayloadFilter", creds);
 
         TraceyFilter tf = new EiffelPayloadRegexFilter(".*KEYWORD.*");
 
-        TraceyTrigger tt = new TraceyTrigger(exchange, upc.getId(), false, false, envKey, Arrays.asList(tf));
+        TraceyTrigger tt = new TraceyTrigger(exchange, creds.getId(), false, false, envKey, Arrays.asList(tf));
         p.getTriggers().put(new TraceyTrigger.TraceyTriggerDescriptor(), tt);
         tt.start(p, true);
-
 
         TraceyRabbitMQSenderImpl sender = new TraceyRabbitMQSenderImpl(traceyHost, user, password, 6666);
         sender.send("Hi there we should trigger. Since we do not include the magic word", "tracey");
         sender.send("This one should be picked up. Since we do include the magic KEYWORD", "tracey");
 
-        r.waitUntilNoActivity();
+        r.waitUntilNoActivityUpTo(timeout);
 
         TraceyAction ta = p.getBuildByNumber(1).getAction(TraceyAction.class);
         assertNotNull(ta);
         assertEquals("This one should be picked up. Since we do include the magic KEYWORD", ta.getMetadata());
 
     }
-
 }
